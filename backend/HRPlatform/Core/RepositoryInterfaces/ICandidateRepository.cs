d87d077c10f0edb0a6b73491ce7d97cd9e9cb252
Pavicevic Tea <pavicevictea6@gmail.com>
2026-05-11 16:37:17 +0200

    feat: add repository interfaces for candidate and skills

---
+++ backend/HRPlatform/Core/RepositoryInterfaces/ICandidateRepository.cs
@@ -0,0 +1,15 @@
+﻿using HRPlatform.Core.Domain;
+
+namespace HRPlatform.Core.RepositoryInterfaces
+{
+    public interface ICandidateRepository
+    {
+        List<Candidate> GetAll();
+        Candidate GetById(int id);
+        Candidate Create(Candidate candidate);
+        Candidate Update(Candidate candidate);
+        void Delete(Candidate candidate);
+        List<Candidate> SearchCandidates(string? name, List<string> skills);
+
+    }
+}
---
+++ backend/HRPlatform/Core/RepositoryInterfaces/ISkillRepository.cs
@@ -0,0 +1,12 @@
+﻿using HRPlatform.Core.Domain;
+
+namespace HRPlatform.Core.RepositoryInterfaces
+{
+    public interface ISkillRepository
+    {
+        List<Skills> GetAll();
+        Skills GetByName(string name);
+        Skills Create(Skills skills);
+        void Delete(Skills skills);
+    }
+}
