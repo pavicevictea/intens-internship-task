8c3284d01428d42177ee37932fb3e7d230925359
Pavicevic Tea <pavicevictea6@gmail.com>
2026-05-11 16:12:47 +0200

    feat: create project and add necessary domain classes

---
+++ backend/HRPlatform/Core/Domain/Candidate.cs
@@ -0,0 +1,24 @@
+﻿namespace HRPlatform.Core.Domain
+{
+    public class Candidate
+    {
+        public int CandidateId { get; private set; }
+        public string FullName { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string ContactNumber { get; private set; }
+        public string Email { get; private set; }
+
+        public ICollection<Skills> Skills { get; private set; } = new List<Skills>();
+
+        private Candidate() { }
+
+        public Candidate(string fullName, DateTime dateOfBirth, string contactNumber, string email)
+        {
+            FullName = fullName;
+            DateOfBirth = dateOfBirth;
+            ContactNumber = contactNumber;
+            Email = email;
+        }
+
+    }
+}
---
+++ backend/HRPlatform/Core/Domain/Skills.cs
@@ -0,0 +1,17 @@
+﻿namespace HRPlatform.Core.Domain
+{
+    public class Skills
+    {
+        public int SkillId { get; private set; }
+        public string Name { get; private set; }
+
+        public ICollection<Candidate> Candidates { get; private set; } = new List<Candidate>();
+
+        private Skills() { }
+
+        public Skills(string name)
+        {
+            Name = name;
+        }
+    }
+}
